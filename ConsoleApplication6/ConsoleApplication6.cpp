e63496bd99c5fe13fd0381b26e3bcddfde86d766
Асафьев Александр <asafjew2015@yandex.ru>
2020-04-26 16:29:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+#include "Windows.h"
+#include <cmath>
+
+using namespace std;
+
+double f1(int x)
+{
+	double r = 0;
+	if (x >= 0) {
+		r = pow(2, -x) + 7 * x;;
+	}
+	else {
+		r = x / 2 + 9;
+	}
+	return r;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	double m, n, y;
+	cout << "Введите первое значение";
+	cin >> m;
+	cout << "Введите количество";
+	cin >> n;
+	cout << "Введите шаг";
+	cin >> y;
+
+	for (int i = 1; i < n; i++, m += y)
+	{
+		double h = f1(m);
+		cout << "i=" << m << ";f1(i)=" << h << endl;
+	}
+	system("pause");
+	return 0;
+}
